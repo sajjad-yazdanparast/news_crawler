015e965601c93152df33464359e396087227357e
sajjad <sajjad.yazdanparast.tehrani@gmail.com>
2020-12-29 23:04:37 +0330

    requirements added. news and website model designed. admin panel registered

--- NewsCrawler/crawler/admin.py
+++ NewsCrawler/crawler/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import News , Website
 # Register your models here.
+
+class NewsAdmin(admin.ModelAdmin) :
+    list_display = ("format_text", "publish_date", "crawl_date" , "get_site_name") 
+
+    def format_text(self, obj) :
+        return str(obj.text)[:100] + " ..."
+
+    def get_site_name(self, obj) :
+        return obj.site.name
+        
+    format_text.short_description = "text"
+    get_site_name.short_description = "site"
+
+class WebsiteAdmin (admin.ModelAdmin) :
+    list_display = ("url", "name") 
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Website, WebsiteAdmin)
+
--- NewsCrawler/crawler/models.py
+++ NewsCrawler/crawler/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Website (models.Model):
+    url = models.URLField(primary_key=True)
+    name = models.TextField()
+    need_for_crawl = models.BooleanField(default=True)
+
+
+class News (models.Model) :
+    text = models.TextField()
+    publish_date = models.DateField()
+    crawl_date = models.DateField(auto_created=True)
+
+    site = models.ForeignKey(Website, on_delete=models.SET_NULL, null=True)
