9d6fa3ef92d30b281a1c82e629691c341a00357a
hossein.ghojavand.27 <hossein.ghojavand.27@gmail.com>
2021-01-13 22:35:09 +0330

    get_rss func fixed

--- NewsCrawler/crawler/views.py
+++ NewsCrawler/crawler/views.py
@@ -68,19 +68,23 @@ class CrawlerResult(APIView) :
     @staticmethod
     def get_rss(base):
         response = ""
-        query = "site:" + base + " rss"
-        address = next(search(query, tld="co.in", num=10, stop=10, pause=2))
-        html_page = urllib.request.urlopen(address)
-        soup = BeautifulSoup(html_page, "html.parser")
-        for link in soup.findAll('a'):
-            l = link.get('href')
-            if l is not None:
-                tx = re.search("^/+.*(rss)+.*", l)
-                if tx and CrawlerResult.is_rss(base + l):
-                    response = base + l
-                    break
+	query = "site:" +base+  " rss"
+	address =next(search(query, tld="co.in", num=10, stop=10, pause=2))
+	html_page = urllib.request.urlopen(address)
+	soup = BeautifulSoup(html_page, "html.parser")
+	for link in soup.findAll('a'):
+		l = link.get('href')
+		if l is not None:
+			if re.search("(rss)+.*",l):
+				if re.search("^/.*" , l):
+					l = base + l
+			else:
+				continue
+			if CrawlerResult.is_rss(l):
+				response = l
+				break
 
-        return response
+	return response
 
     @staticmethod
     def is_rss(url):
